82006e9a3bc1bf99e6e0febfdfcfc8d70e1b018f
Bryan <bdavis@artandbusinessone.com>
2021-09-14 15:08:47 -0600

    alignment of button

--- wp-content/themes/markelhomes2021/page-templates/page-communities.php
+++ wp-content/themes/markelhomes2021/page-templates/page-communities.php
@@ -31,7 +31,7 @@
             <?php echo get_field('square_footage') ?><br />
             Priced from $<?php echo get_field('pricing'); ?>
           </p>
-          <p><a href="<?php the_permalink(); ?>" title="<?php the_title() ?>" class="btn outline-btn gold-txt community-btn">visit community</a></p>
+          <p style="align-self: flex-end;"><a href="<?php the_permalink(); ?>" title="<?php the_title() ?>" class="btn outline-btn gold-txt community-btn">visit community</a></p>
         </div>
       </article>
       <?php endwhile; endif; wp_reset_query(); ?>
