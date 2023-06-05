9a8af4fcef81a99fdad79bcc7eab3106f87777e7
bdavis-aabo <bdavis@artandbusinessone.com>
2023-06-05 14:40:27 -0600

    Blog page updates

--- wp-content/themes/markelhomes2021/index.php
+++ wp-content/themes/markelhomes2021/index.php
@@ -23,9 +23,11 @@
     <?php while(have_posts()): the_post(); ?>
       <article class="news-article" id="<?php echo 'news-article-' . $post->ID ?>">
         <?php if(have_rows('featured_image')): while(have_rows('featured_image')): the_row(); $_thumb = get_sub_field('thumbnail_image'); ?>
-        <figure class="article-thumb">
-          <img src="<?php echo $_thumb['url'] ?>" alt="<?php the_title() ?>" class="img-fluid" />
-        </figure>
+        <a href="<?php the_permalink() ?>" title="<?php the_title() ?>">
+					<figure class="article-thumb">
+          	<img src="<?php echo $_thumb['url'] ?>" alt="<?php the_title() ?>" class="img-fluid" />
+        	</figure>
+				</a>
         <?php endwhile; endif; ?>
         <h2 class="article-title"><?php the_title(); ?></h2>
         <a href="<?php the_permalink() ?>" title="<?php the_title() ?>" class="article-link gold-txt">Read More</a>
